639a96471d44021b18ba21139e808df5a8cbd131
Raphael Collet <rco@odoo.com>
2017-08-16 16:55:09 +0200

    [IMP] backoprt avoid one query when recomputing a field (#18846)
    Specifically, instead of reading fields on , simply read those
    fields and catch .  This generates around 5% less queries

--- odoo/models.py
+++ odoo/models.py
@@ -3593,6 +3593,8 @@ class BaseModel(object):
     @api.multi
     def _write(self, vals):
         # low-level implementation of write()
+        if not self:
+            return True
         self.check_field_access_rights('write', list(vals))
 
         cr = self._cr
@@ -5331,13 +5333,23 @@ class BaseModel(object):
             ns = [f.name for f in fs if f.store]
             # evaluate fields, and group record ids by update
             updates = defaultdict(set)
-            for rec in recs.exists():
-                vals = rec._convert_to_write({n: rec[n] for n in ns})
+            for rec in recs:
+                try:
+                    vals = {n: rec[n] for n in ns}
+                except MissingError:
+                    continue
+                vals = rec._convert_to_write(vals)
                 updates[frozendict(vals)].add(rec.id)
             # update records in batch when possible
             with recs.env.norecompute():
-                for vals, ids in updates.iteritems():
-                    recs.browse(ids)._write(dict(vals))
+                for vals, ids in updates.iteritems(updates):
+                    target = recs.browse(ids)
+                    try:
+                        target._write(dict(vals))
+                    except MissingError:
+                        # retry without missing records
+                        target.exists()._write(dict(vals))
+
             # mark computed fields as done
             map(recs._recompute_done, fs)
 
