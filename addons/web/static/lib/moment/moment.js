de76ca3aa98c450acb6e16d9ef720f47b309de40
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2017-11-15 12:14:32 +0100

    [CHG] Set monday as the first day of the week by default.

--- addons/web/static/lib/moment/moment.js
+++ addons/web/static/lib/moment/moment.js
@@ -954,8 +954,8 @@
         },
 
         _week : {
-            dow : 0, // Sunday is the first day of the week.
-            doy : 6  // The week that contains Jan 1st is the first week of the year.
+            dow : 1, // Sunday is the first day of the week.
+            doy : 4  // The week that contains Jan 1st is the first week of the year.
         },
 
         _invalidDate: 'Invalid date',
